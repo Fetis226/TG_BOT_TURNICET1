8849fe99d8a08cb7b4ecd8e84ef3c07cd92b4d8a
Fetis <86sdi@mail.ru>
2024-05-16 23:32:21 +0300

    Функция мэйн, тело бота

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ from config_reader import config
 from handlers import router
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
+async def main():
+    dp = Dispatcher()
+    dp.startup.register(on_startup)
+    dp.include_router(handlers.router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+async def on_startup():
+    asyncio.create_task(Send(types, bot))
+
+if __name__ == "__main__":
+     asyncio.run(main())
