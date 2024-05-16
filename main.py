5ec393d418c5d2c2675c1ef72f9500ec99718ec3
Fetis <86sdi@mail.ru>
2024-05-16 23:29:21 +0300

    Импорты мэйн, инициализация бота. Начало создания проекта

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from handlers import Send
+import handlers
+from BD import parent_log_reg, rassilka, entry, check
+from aiogram.filters.command import Command
+from aiogram.types import Message
+from config_reader import config
+from handlers import router
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
