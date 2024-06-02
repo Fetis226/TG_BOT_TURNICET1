4a542abef8f6cae06ed7ef4bfef1d82606141b32
Fetis <86sdi@mail.ru>
2024-06-02 19:18:16 +0300

    Исправление ошибок и фильтрация sa_instance в выгрузке логов

--- encoder.py
+++ encoder.py
@@ -3,6 +3,7 @@ import sqlalchemy as db
 import json
 import os
 import io
+import copy
 import pandas as pd
 from sqlalchemy import create_engine, desc, func
 from sqlalchemy.orm import Session
@@ -18,14 +19,40 @@ def alchemyencoder(obj):
 def export_log():
     l = open('txt.txt', "r")
     last_id = int(l.read())
+    os.remove("outputs1.xlsx")
     with Session(autoflush=False, bind=engine) as db:
         with open('json.json', 'w') as f:
             Log = db.query(log).filter(log.idlog > last_id).order_by(log.idlog).all()
-            print(Log)
-            os.remove("outputs1.xlsx")
             result_dict = [row.__dict__ for row in Log]
             print(result_dict)
-            pd.DataFrame(result_dict).to_excel('outputs1.xlsx')
+            result_dict_copy = copy.deepcopy(result_dict)
+            if not result_dict == False:
+                print(result_dict)
+                a=0
+                for i in list(result_dict_copy):
+                    for j in i:
+                        print(j, a,"<>", len(result_dict))
+                        if j == "_sa_instance_state":
+                            result_dict[a].pop(j)
+                        print(result_dict[a])
+                    a+=1
+                    if a+1 > len(result_dict):
+                        break
+                a=0
+                print(result_dict,"CHECK LENGHT", len(result_dict))
+                for i in result_dict:
+                    for j in i:
+                        if j =="date":
+                            print(j)
+                            result_dict[a].update(date=result_dict[a].get("date").strftime('%d-%m-%Y %H:%M:%S'))
+                        print("---- final",j, a, "<>", len(result_dict), result_dict[a], result_dict,"--- final")
+                    a+=1
+                    if a + 1 > len(result_dict):
+                        break
+                print(result_dict,"final result dict")
+                pd.DataFrame(result_dict).to_excel('outputs1.xlsx')
+            if not result_dict == True:
+                print("Обновлений нет")
             f.close()
 def update_student():
     with Session(autoflush=False, bind=engine) as db:
@@ -71,3 +98,4 @@ def update_raspisan():
                 if a+1>len(json_data):
                     break
             json_data.clear()
+export_log()
